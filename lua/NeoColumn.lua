c2e0a1cfd49bc3a7ed9f641ca0250a467d316b19
Robert Sears <rob@MacBook-Air-10.home>
2023-03-23 15:58:50 -0500

    ...

--- lua/NeoColumn.lua
+++ lua/NeoColumn.lua
@@ -13,11 +13,10 @@
 local M = {}
 local fn = vim.fn
 local cmd = vim.cmd
-local enabled_bufs = {}
+local enabled_bufs = vim.g.neo_column_enabled_bufs or {}
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 local user_cmd = vim.api.nvim_create_user_command
-
 
 M.config = {
   notify = true,
@@ -34,8 +33,14 @@ M.setup = function(user_settings)
     M.config[k] = v
   end
 
+  -- Store the enabled buffers in a global variable
+  vim.g.neo_column_enabled_bufs = enabled_bufs
+
   -- Toggle-NeoColumn
   user_cmd("ToggleNeoColumn", "lua require('NeoColumn').toggle_NeoColumn()", {})
+
+  -- Clear-NeoColumnList
+  user_cmd("ClearNeoColumn", "lua require('NeoColumn').clear_enabled_list()", {})
 
   -- Apply-NeoColumn
   autocmd({ "BufEnter", "BufWinEnter" }, {
@@ -51,11 +56,21 @@ end
 -- Toggle-NeoColumn
 function M.toggle_NeoColumn()
   if M.excluded_bufs() then return end
-  enabled_bufs[fn.bufnr('%')] = not enabled_bufs[fn.bufnr('%')]
+  local file_path = fn.expand('%:p')
+  enabled_bufs[file_path] = not enabled_bufs[file_path]
   if M.config.notify then
     M.notify_NeoColumn()
   end
   M.apply_NeoColumn()
+end
+
+-- Clear-Enabled-List
+function M.clear_enabled_list()
+  enabled_bufs = {}
+  vim.g.neo_column_enabled_bufs = enabled_bufs
+  if M.config.notify then
+    vim.notify("NeoColumn enabled list cleared")
+  end
 end
 
 -- Excluded-Buf
@@ -66,25 +81,12 @@ end
 
 -- Notify-NeoColumn
 function M.notify_NeoColumn()
-  vim.notify("NeoColumn " .. (enabled_bufs[fn.bufnr('%')] and "Enabled" or "Disabled"))
+  vim.notify("NeoColumn " .. (enabled_bufs[fn.expand('%:p')] and "Enabled" or "Disabled"))
 end
 
 -- Apply-NeoColumn
 function M.apply_NeoColumn()
   local NeoColumn = M.config.NeoColumn
-  local fg_color = M.config.fg_color
-  local bg_color = M.config.bg_color
-  local current_ft = vim.bo.filetype
-
-  if M.config.custom_NeoColumn[current_ft] then
-    NeoColumn = M.config.custom_NeoColumn[current_ft]
-  end
-
-  cmd("silent! highlight ColorColumn guifg=" .. fg_color .. " guibg=" .. bg_color .. " | call clearmatches()")
-  if not M.excluded_bufs() and enabled_bufs[fn.bufnr('%')] then
-    fn.matchadd("ColorColumn", "\\%" .. NeoColumn .. "v.",
-      100)
-  end
-end
+  local fg
 
 return M
