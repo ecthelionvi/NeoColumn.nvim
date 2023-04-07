589d3cc4ac5b6a4c7aaa3ed3a92b5f7ed9753102
Robert Sears <rob@MacBook-Air-10.home>
2023-04-06 23:24:01 -0500

    Refactoring

--- lua/NeoColumn.lua
+++ lua/NeoColumn.lua
@@ -53,6 +53,7 @@ end
 local neocolumn_bufs = load_NeoColumn()
 
 NeoColumn.setup = function(user_settings)
+  user_settings = user_settings or {}
   -- Merge user settings with default settings
   for k, v in pairs(user_settings) do
     config[k] = v
